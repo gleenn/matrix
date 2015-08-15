6ebdfdbaf3900286e9298a00c4cd0488c2b83f4e
Glenn <gleenn@gmail.com>
2015-08-15 01:35:38 -0700

    yo yo yo don't reverse cuz its slow but not really

--- src/matrix/core.clj
+++ src/matrix/core.clj
@@ -4,16 +4,18 @@
     [clojure.core.matrix :as m]))
 
 (defn fib [size]
-  (reverse (loop [a 0 b 1 count 0 l (list)]
-             (if (< count size)
-               (recur b
-                      (mod (+' a b) (int (/ Integer/MAX_VALUE 10)))
-                      (inc count)
-                      (conj l a))
-               l))))
+  (loop [a 0 b 1 count 0 l (list)]
+    (if (< count size)
+      (recur b
+             (mod (+' a b) (int (Math/sqrt Integer/MAX_VALUE)))
+             (inc count)
+             (conj l a))
+      l)))
 
 (defn foo [& args]
-  (let [m (take 100 (fib 200)) v (fib 100)]
-    (pprint (m/mmul m v))))
+  (time (let [size 1000000
+              m (take size (fib (inc' size)))
+              v (fib size)]
+          (pprint (time (m/mmul m v))))))
 
 (foo)
